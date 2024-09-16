6519946fdd9258f62a4c2783e0b18db58accf6d6
KuoLinTang <linkuotang1997@gmail.com>
2024-09-16 22:39:20 +0100

    Added edit & delete button. Enabled delete function. Enabled isCompleted function.

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -1,7 +1,22 @@
-function ToDo({todo}){
-    return <div className="todo">
-        <p>{todo}</p>
+import { MdDelete, MdEdit } from "react-icons/md";
+
+function ToDo({ todo, deleteTodo, toggleCompleted }) {
+  const handleClick = () => {
+    deleteTodo(todo.id);
+  };
+
+  return (
+    <div className={`todo ${todo.isCompleted ? 'completed': ''}`}>
+      <p onClick={() => {toggleCompleted(todo.id)}}>{todo.content}</p>
+      <div>
+        <MdEdit style={{ cursor: "pointer" }} />
+        <MdDelete
+          style={{ cursor: "pointer", marginLeft: "5px" }}
+          onClick={handleClick}
+        />
+      </div>
     </div>
+  );
 }
 
-export default ToDo
+export default ToDo;
